38586982b1d388ced683ab87af0a0b4f1da1b4de
DaryaV95 <dpresnova95@gmail.com>
2021-01-30 18:41:43 +0300

    Добавила две функции на получение случайного числа

---
+++ js/main.js
@@ -0,0 +1,21 @@
+'use strict';
+//Взято с MDN. Получение случайного целого числа в заданном интервале.
+const getRandomIntInclusive = function (min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  if (min >= 0, max >= 0) {
+    return (max < min) ? null : Math.floor(Math.random() * (max - min + 1)) + min; //Максимум и минимум включаются
+  }
+  return 0;
+}
+getRandomIntInclusive(1, 56);
+
+//Получение случайного числа с плавающей точкой из переданного диапазона включительно.
+function getRandomArbitrary(min, max, number) {
+  if (min >= 0, max >= 0) {
+    let randomNumber = (max < min) ? null : Math.floor(Math.random() * (max - min + 1)) + min; //Максимум и минимум включаются
+    return randomNumber.toFixed(number);
+  }
+  return 0;
+}
+getRandomArbitrary(1.6575, 35.7586, 2);
