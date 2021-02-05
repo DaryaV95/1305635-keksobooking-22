dd613a7573f81639e4a7aa2d1db6d3077322d856
DaryaV95 <dpresnova95@gmail.com>
2021-02-05 22:22:51 +0300

    Задание Больше деталей

--- js/main.js
+++ js/main.js
@@ -1,4 +1,66 @@
 'use strict';
+
+const TITLES = [
+  'Квартира целиком',
+  'Дом, в котором бьётся пульс города',
+  'Уютное бунгало',
+  'Ослепительный дворец',
+];
+
+const TYPES = [
+  'palace',
+  'flat',
+  'house',
+  'bungalow',
+];
+
+const ROOMS = ['1', '2', '3', '4', '5'];
+
+const GUESTS = ['1', '2', '3', '4', '5', '6']
+
+const TIMES_CHECKIN = [
+  '12:00',
+  '13:00',
+  '14:00',
+];
+
+const TIMES_CHECKOUT = [
+  '12:00',
+  '13:00',
+  '14:00',
+];
+
+const FEATURES = [
+  'wifi',
+  'dishwasher',
+  'parking',
+  'washer',
+  'elevator',
+  'conditioner',
+];
+
+const DESCRIPTIONS = [
+  'Невероятный дворец с аквадискотекой и маленьким кинотеатром',
+  'Светлая и просторная квартира с видом на парк',
+  'Небольшой домик в традиционном стиле, но с приятной гостиной',
+  'Уютное бунгало в центре города с прекрасным садом',
+];
+
+const PHOTOS = [
+  'http://o0.github.io/assets/images/tokyo/hotel1.jpg',
+  'http://o0.github.io/assets/images/tokyo/hotel2.jpg',
+  'http://o0.github.io/assets/images/tokyo/hotel3.jpg',
+];
+
+const OBJECT_COUNT = 10;
+const MIN_PRICE = 1000;
+const MAX_PRICE = 1000000;
+const MIN_POINT_X = 35.65000;
+const MAX_POINT_X = 35.70000;
+const MIN_POINT_Y = 139.70000;
+const MAX_POINT_Y = 139.80000;
+const FLOAT_COUNT = 5;
+
 //Взято с MDN. Получение случайного целого числа в заданном интервале.
 const getRandomIntInclusive = function (min, max) {
   min = Math.ceil(min);
@@ -8,7 +70,6 @@ const getRandomIntInclusive = function (min, max) {
   }
   return 0;
 }
-getRandomIntInclusive(1, 56);
 
 //Получение случайного числа с плавающей точкой из переданного диапазона включительно.
 const getRandomArbitrary = function (min, max, number) {
@@ -19,4 +80,51 @@ const getRandomArbitrary = function (min, max, number) {
   }
   return 0;
 }
-getRandomArbitrary(1.6575, 35.7586, 2);
+
+//Функция случайного элемента из массива
+const getRandomArrayElement = (elements) => {
+  return elements[getRandomIntInclusive(0, elements.length - 1)];
+};
+
+//Функция добавления случайного элемента в массив
+const getRandomArray = (elements) => {
+  let getArr = [];
+  for(let i = 0; i < elements.length - 1; i++) {
+    if (getRandomIntInclusive(0, 1) === true) {
+      getArr.push(elements[i]);
+    }
+  }
+  return getArr;
+};
+
+const createObject = () => {
+  const getLocation = {
+    x: getRandomArbitrary(MIN_POINT_X, MAX_POINT_X, FLOAT_COUNT),
+    y: getRandomArbitrary(MIN_POINT_Y, MAX_POINT_Y, FLOAT_COUNT),
+  }
+  return {
+    author: {
+      avatar: `img/avatars/user0${getRandomIntInclusive(1, 8)}.png`,
+    },
+    offer: {
+      title: getRandomArrayElement(TITLES),
+      address: `location.${getLocation.x}, location.${getLocation.y}`,
+      price: getRandomIntInclusive(MIN_PRICE, MAX_PRICE),
+      type: getRandomArrayElement(TYPES),
+      rooms: getRandomArrayElement(ROOMS),
+      guests: getRandomArrayElement(GUESTS),
+      checkin: getRandomArrayElement(TIMES_CHECKIN),
+      checkout: getRandomArrayElement(TIMES_CHECKOUT),
+      features: getRandomArray(FEATURES),
+      description: getRandomArrayElement(DESCRIPTIONS),
+      photos: getRandomArray(PHOTOS),
+    },
+    location: {
+      x: getLocation.x,
+      y: getLocation.y,
+    },
+  };
+};
+
+const similarArray = new Array(OBJECT_COUNT).fill(null).map(() => createObject());
+similarArray; //чтобы линтер не ругался
