c821694ba29075f19384028d32536b29e533ccc8
DaryaV95 <dpresnova95@gmail.com>
2021-02-28 19:28:06 +0300

    Помощь друга (часть 2)

--- js/form.js
+++ js/form.js
@@ -1,3 +1,6 @@
+const MIN_NAME_LENGTH = 30;
+const MAX_NAME_LENGTH = 100;
+
 const typeOfHousing = document.querySelector('#type');
 const priceOfHousing = document.querySelector('#price');
 const timeIn = document.querySelector('#timein');
@@ -30,3 +33,50 @@ timeIn.addEventListener('change', () => {
 timeOut.addEventListener('change', () => {
   timeIn.value = timeOut.value;
 });
+
+//проверка поля объявления
+const userAdForm = document.querySelector('#title');
+
+userAdForm.addEventListener('input', () => {
+  const valueLength = userAdForm.value.length;
+
+  if (valueLength < MIN_NAME_LENGTH) {
+    userAdForm.classList.add('ad-form__error');
+    userAdForm.setCustomValidity('Объявление должно состоять минимум из 30-ти символов');
+  } else if (valueLength > MAX_NAME_LENGTH) {
+    userAdForm.classList.add('ad-form__error');
+    userAdForm.setCustomValidity('Удалите лишние ' + (valueLength - MAX_NAME_LENGTH) +' симв.');
+  } else {
+    userAdForm.classList.remove('ad-form__error');
+    userAdForm.setCustomValidity('');
+  }
+  userAdForm.reportValidity();
+});
+
+//проверка полей комнат и гостей
+const rooms = document.querySelector('#room_number');
+const capacity = document.querySelector('#capacity');
+
+const checkGuest = () => {
+  const roomsValue = Number(rooms.value);
+  const guestsValue = Number(capacity.value);
+
+  if (roomsValue !== 100 && guestsValue === 0) {
+    capacity.setCustomValidity('Для этого варианта мало гостей');
+  } else if (roomsValue < guestsValue) {
+    capacity.setCustomValidity('Для этого варианта слишком много гостей')
+  } else if (roomsValue === 100 && guestsValue !== 0) {
+    capacity.setCustomValidity('Вариант не подходит для гостей')
+  } else {
+    capacity.setCustomValidity('');
+  }
+  capacity.reportValidity();
+}
+
+capacity.addEventListener('change', () => {
+  checkGuest();
+})
+
+rooms.addEventListener('change', () => {
+  checkGuest();
+})
