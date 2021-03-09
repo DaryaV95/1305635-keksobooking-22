1bf81b8bbacf9c69a337c88830163ecda210a91b
DaryaV95 <dpresnova95@gmail.com>
2021-03-09 23:56:09 +0300

    Работа с формой

--- js/main.js
+++ js/main.js
@@ -5,6 +5,7 @@ import './create-map.js';
 import './api.js';
 import {getData} from './api.js';
 import {createOffers} from './create-map.js';
+import './submit.js';
 
 getData((offers) => {
   createOffers(offers);
--- js/submit.js
+++ js/submit.js
@@ -1,1 +1,75 @@
+import {sendData} from './api.js';
+import {mainMarker} from './create-map.js';
+import {adForm, mapFilters} from './disable-form.js';
+import {CENTER_TOKYO_LAT, CENTER_TOKYO_LNG} from './const.js';
 
+const adFormReset = adForm.querySelector('.ad-form__reset');
+const main = document.querySelector('main');
+const success = document.querySelector('#success').content.querySelector('div');
+
+const isEscEvent = (evt) => {
+  return evt.key === 'Esc' || evt.key === 'Escape';
+};
+
+const successMessage = () => {
+  const element = success.cloneNode(true);
+  main.append(element);
+
+  document.addEventListener('keydown', () => {
+    if (isEscEvent) {
+      element.remove();
+    }
+  });
+
+  document.addEventListener('click', () => {
+    element.remove();
+  });
+}
+
+const resetFunction = () => {
+  adForm.reset();
+  mainMarker.setLatLng([CENTER_TOKYO_LAT, CENTER_TOKYO_LNG]);
+  mapFilters.reset();
+};
+
+adFormReset.addEventListener('click', () => {
+  resetFunction();
+});
+
+const error = document.querySelector('#error').content.querySelector('div');
+
+const errorMessage = () => {
+  const element = error.cloneNode(true);
+  main.append(element);
+
+  const errorButton = element.querySelector('.error__button');
+
+  errorButton.addEventListener('click', () => {
+    element.remove();
+  });
+
+  document.addEventListener('keydown', () => {
+    if (isEscEvent) {
+      element.remove();
+    }
+  });
+
+  document.addEventListener('click', () => {
+    element.remove();
+  });
+}
+
+const setFormSubmit = (onSuccess, onFail) => {
+
+  adForm.addEventListener('submit', (evt) => {
+    evt.preventDefault();
+
+    sendData(
+      () => onSuccess(),
+      () => onFail(),
+      new FormData(evt.target),
+    );
+  });
+};
+
+setFormSubmit(successMessage, errorMessage);
